5765c42832937eb655497b0b9890f8f09b4d2d4f
he bo <dooacontrol@protonmail.com>
2025-03-28 12:39:25 +0800

    Feat: WPF Control project start Learning WPF for a week, deside create a WPF version of the application, start with control. 1, catagroy projects. 2, create control project and testbench project of WPF version. 3, implement SearchLayer class and part of SearchConfig class.

---
+++ configControlWpf/SearchConfig.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace configControlWpf
+{
+    /// <summary>
+    /// Interaction logic for SearchConfig.xaml
+    /// </summary>
+    public partial class SearchConfig : UserControl
+    {
+        public SearchConfig()
+        {
+            InitializeComponent();
+            addSearchLayer();
+        }
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            addSearchLayer();
+        }
+        private void addSearchLayer()
+        {
+            SearchLayer newSl = new SearchLayer();
+            panelSearchLayers.Children.Add(newSl);
+            newSl.GotFocus+= new RoutedEventHandler((sender, e) => {
+                SearchLayer focusedSearchLayer = (SearchLayer)sender;
+                focusedSearchLayer.Background = newSl.focusBrush;
+                panelSearchLayers.Tag = newSl;
+                foreach(SearchLayer sl in panelSearchLayers.Children)
+                {
+                    if (sl != focusedSearchLayer)
+                    {
+                        sl.Background = sl.unfocusBrush;
+                    }
+                }
+            });
+        }
+        private void DeleteCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (panelSearchLayers !=null 
+                && panelSearchLayers.Tag is SearchLayer);
+        }
+        private void DeleteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SearchLayer focusedSearchLayer = (SearchLayer)panelSearchLayers.Tag;
+            panelSearchLayers.Children.Remove(focusedSearchLayer);
+            panelSearchLayers.Tag = null;
+        }
+    }
+}
---
+++ configControlWpf/SearchLayer.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using xy.scraper.page.parserConfig;
+
+namespace configControlWpf
+{
+    /// <summary>
+    /// Interaction logic for SearchLayer.xaml
+    /// </summary>
+    public partial class SearchLayer : UserControl
+    {
+        public Brush focusBrush = Brushes.LightBlue;
+        public Brush unfocusBrush;
+        public SearchLayer()
+        {
+            InitializeComponent();
+            unfocusBrush = this.Background;
+        }        
+        private void mouseUp(object sender, RoutedEventArgs e)
+        {
+            txtStart.Focus();
+        }
+        private void getFocus(object sender, RoutedEventArgs e)
+        {
+            //this.Background = focusBrush;
+        }
+        private void lostFocus(object sender, RoutedEventArgs e)
+        {
+            //this.Background = unfocusBrush;
+        }
+
+        public string Start
+        {
+            get
+            {
+                return txtStart.Text;
+            }
+
+            set
+            {
+                txtStart.Text = value;
+            }
+        }
+        public string End
+        {
+            get
+            {
+                return txtEnd.Text;
+            }
+
+            set
+            {
+                txtEnd.Text = value;
+            }
+        }
+
+        public JsonObject JsonObj
+        {
+            get
+            {
+                JsonObject json = new JsonObject();
+                json[JCfgName.start] = Start;
+                json[JCfgName.end] = End;
+                return json;
+            }
+
+            set
+            {
+                Start = value[JCfgName.start].GetValue<String>();
+                End = value[JCfgName.end].GetValue<String>();
+            }
+        }
+
+        private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
